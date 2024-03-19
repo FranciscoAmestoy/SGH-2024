032202db2c3f5e65cb21e6fdb28f5f2497775fee
Francisco Amestoy <flyc0de92@gmail.com>
2024-03-18 22:41:36 -0300

    Created Landing & Logind Drafts

---
+++ src/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import './Landing.css'
+
+function Landing() {
+  return (
+  <div className="landingCanvas">
+    <div className='login'>Landing</div>
+  </div>
+    
+  )
+}
+
+export default Landing
